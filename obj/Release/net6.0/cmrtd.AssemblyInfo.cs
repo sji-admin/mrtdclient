851081762ff9fcacec565cc1bbf7836bebc515a6
Farhan <saputrafarhan32@gmail.com>
2025-11-19 23:32:17 +0700

    handle error scan

--- obj/Release/net6.0/cmrtd.AssemblyInfo.cs
+++ obj/Release/net6.0/cmrtd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cmrtd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54dfefde9df35bb0f816ffee41add77524db75f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc02854752288d902108656a533596f130042b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("cmrtd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cmrtd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
